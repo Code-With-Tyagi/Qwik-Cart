d0ec82d3bcd925b592f77e5c3be8e7acec10fed0
vansh <4530vanshtyagi@gmail.com>
2026-08-06 10:54:31 +0530

    some changes

--- Backend/src/services/email.service.js
+++ Backend/src/services/email.service.js
@@ -3,9 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+import dns from "dns";
+
+dns.setDefaultResultOrder("ipv4first");
+
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
+    port: 465,
     secure: true,
     auth: {
         user: process.env.EMAIL_USER,
